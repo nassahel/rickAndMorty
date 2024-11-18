079dc155a696c44cade0f5ffbdcbf024ea9475c3
nassahel <nassahel.elias@gmail.com>
2024-11-17 22:59:21 -0300

    limpiza de archivos no usados

--- src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-export const authenticate = async () => {
-    const usersRegistered = await JSON.parse(localStorage.getItem('users'));
-    const loggedUser = await JSON.parse(localStorage.getItem('LSloggedUser'));
-    if (usersRegistered && loggedUser) {
-        const isLogged = usersRegistered.some((existingUser) => existingUser.email === loggedUser.email && existingUser.pass === loggedUser.pass)
-        return isLogged
-
-    }
-}
